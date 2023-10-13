9913947fb37391835d18a54dbe344fd56c8b0c5e
cezary <cezary.szukiel@gmail.com>
2023-10-13 14:31:07 +0200

    ..

---
+++ __main__.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+msg = input('zadaj pytanie: \n')
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": msg
+        }
+    ]
+)
+
+print(completion.choices[0].message)
